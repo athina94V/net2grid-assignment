f0501da4872b12fb9f4be76c6630bebaf079000a
athina94V <49948038+athina94V@users.noreply.github.com>
2019-05-01 13:01:32 +0300

    format code

--- index.php
+++ index.php
@@ -12,36 +12,8 @@ while ($i < 10) {
     $response = file_get_contents($ini_array['hostname']);
     $response = json_decode($response);
 
-
-
-
     send::sendMessage($response,$message_queue);
     receive::receiveMessage($message_queue);
     $i++;
 }
-////// Create connection
-//      $hostname = "candidaterds.n2g-dev.net";
-//       $username = "candidate";
-//        $password = "9jcnMWrjf6zBFLmwjfYEVRb6";
-//        $conn = new mysqli($hostname, $username, $password);
-////// Check connection
-//        if ($conn->connect_error) {
-//            die("Connection failed: " . $conn->connect_error);
-//        }
-//
-////// Print connection details
-//        print_r("\n");
-//        print_r($conn);
-//
-//        $databases = mysqli_query($conn, "SHOW DATABASES");
-//
-////// Print result of query
-//        print_r("\n");
-//        print_r($databases);
-//        print_r("\n");
-//
-////// Print each item of array
-//        while ($row = mysqli_fetch_array($databases)) {
-//            print_r($row[0] . "\n");
-//        }
-//
+
--- receive.php
+++ receive.php
@@ -17,7 +17,6 @@ class receive {
         $connection = new AMQPStreamConnection($message_queue['hostname'], $message_queue['port'], $message_queue['username'], $message_queue['password']);
         $channel = $connection->channel();
 
-
         $channel->exchange_declare($exchange_name, $exchange_type, false, true, false);
         list ($queue_name,, ) = $channel->queue_declare($queue_name, false, true, false, false, false, $ttl);
         $binding_key = '#.#.#.#.#';
@@ -26,9 +25,9 @@ class receive {
 
 
         $callback = function ($msg) {
-
             receive::insertRecord($msg);
         };
+        
         $channel->basic_consume($queue_name, '', false, true, false, false, $callback);
 
         if (count($channel->callbacks)) {
@@ -45,12 +44,10 @@ class receive {
 
         $conn = new mysqli($database_details['servername'], $database_details['username'], $database_details['password'], $database_details['dbname'], 3306);
 
-
         $value = substr($msg->body, 0, ($msg->body_size) - 13);
         $timestamp = substr($msg->body, ($msg->body_size) - 13);
         $sql = "INSERT INTO results (timestamp, value) VALUES('$timestamp', '$value')";
         if ($conn->query($sql) !== TRUE) {
-
             echo "Error creating row " . $conn->error;
         }
         $conn->close();
